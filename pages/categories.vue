274b6bb83a24816f8646fde0fb071bcde305dd84
willmarcel <marcell.luh@gmail.com>
2022-03-03 15:40:20 -0300

    Pratico 02 - Categorias - Update, Delete e Reload

--- pages/categories.vue
+++ pages/categories.vue
@@ -10,10 +10,10 @@
       <div>
         <div class="flex items-center space-x-3">
           <div>
-            <AppFormInput />
+            <AppFormInput v-model="name" @keyup.enter="addCategory" />
           </div>
 
-          <AppButton>
+          <AppButton @click="addCategory">
             Adicionar
           </AppButton>
         </div>
@@ -37,40 +37,25 @@
         </tr>
         </thead>
         <tbody class="bg-white divide-y divide-gray-200">
-        <tr class="bg-white">
-          <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-            <div class="w-72">
-              <AppFormInput />
-            </div>
-          </td>
-
-          <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium space-x-4">
-            <a
-              href="#"
-              class="text-indigo-600 hover:text-indigo-900"
-            >Edit
-            </a>
-
-            <a
-              href="#"
-              class="text-red-600 hover:text-red-900"
-            >Excluir
-            </a>
-          </td>
-        </tr>
-
         <tr
           v-for="category in categories"
           :key="category.id"
           class="bg-white">
           <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-            {{ category.name }}
+            <div
+              v-if="category.is_updating"
+              class="w-72"
+            >
+              <AppFormInput v-model="category.name" @keyup.enter="updateCategory(category)" />
+            </div>
+            <template v-else>{{ category.name }}</template>
           </td>
 
           <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium space-x-4">
             <a
               href="#"
               class="text-indigo-600 hover:text-indigo-900"
+              @click.stop.prevent="toUpdate(category)"
             >Edit
             </a>
 
@@ -105,16 +90,50 @@ export default {
   async asyncData({ store }) {
     return {
       categories: await store.dispatch('categories/getCategories')
+      //Atribuindo estado no objeto para identificar que o msm está para edição
+      .then(response => response.map(o => ({ ...o, is_updating: false })))
     }
   },
 
   data() {
-    return {};
+    return {
+      name: ''
+    };
   },
 
   methods: {
     deleteCategory(id) {
-      this.$store.dispatch('categories/deleteCategory', id);
+      this.$store.dispatch('categories/deleteCategory', id)
+      .then(() => {
+        const idx = this.categories.findIndex(o => o.id === id);
+        this.categories.splice(idx, 1);
+      });
+    },
+    toUpdate(category) {
+      category.is_updating = true;
+    },
+    updateCategory(category) {
+      const data = {
+        name: category.name
+      };
+      this.$store.dispatch('categories/updateCategory', {id: category.id, data})
+      .then(() => {
+        category.is_updating = false;
+      });
+    },
+    addCategory() {
+      if(!this.name) {
+        return
+      }
+
+      const data = {
+        name: this.name
+      };
+      this.$store.dispatch("categories/addCategory", data)
+      .then((response) => {
+        this.categories.push(response);
+        this.name = '';
+      });
     }
   },
 };
--- store/categories/actions.js
+++ store/categories/actions.js
@@ -4,5 +4,11 @@ export default {
   },
   deleteCategory(context, id) {
     return this.$axios.$delete(`categories/${id}`)
+  },
+  updateCategory(context, { id, data }) {
+    return this.$axios.$patch(`categories/${id}`, data)
+  },
+  addCategory(context, data) {
+    return this.$axios.$post('categories', data)
   }
 }
